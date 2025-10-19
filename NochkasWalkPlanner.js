7cce17d829fefa0df93e80214c742ab21ab1ca14
BeeCee <154042485+nadya-lynn@users.noreply.github.com>
2025-10-19 16:54:48 -0500

    Add Nochka's Walk Planner Application first commit

---
+++ NochkasWalkPlanner.js
@@ -0,0 +1,11 @@
+// --------------------------------
+// Nochka' Walk Planner Application
+// --------------------------------
+// Nochka is a 4 years old pit bull rescue pup, and my life and my love. 
+
+// Values, Datatypes, and Operations.  Store and calcutale total walk time.
+
+let morningWalk = 30; // minutes.
+let eveningWalk = 60; 
+let totalWalkTime = morningWalk + eveningWalk; // using number operation.
+console.log("Good job, Nochka! Total walk time today:", totalWalkTime, "minutes");
